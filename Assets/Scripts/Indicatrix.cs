79b82c1d0d4cd8e614ce2be259a72b812fc14eea
DOD14 <38224694+DOD14@users.noreply.github.com>
2020-12-12 22:21:46 +0200

    Princpal RIs added

--- Assets/Scripts/Indicatrix.cs
+++ Assets/Scripts/Indicatrix.cs
@@ -5,36 +5,29 @@ using UnityEngine;
 public class Indicatrix : MonoBehaviour
 {
 
-    public float alpha = 1;
-    public float beta = 2;
-    public float gamma = 3;
+    public float[] RIs = { 1, 2, 3 };
+    public float rotationSpeed = 1f;
 
     private Transform _transform;
 
     private void OnEnable()
     {
 
-// enforce alpha <= beta <= gamma
-        if (alpha > beta)
-        {
-            float temp = alpha;
-            alpha = beta;
-            beta = temp;
-        }
 
-        if(gamma < beta)
-        { float temp = beta;
-            beta = gamma;
-            gamma = temp;
-        }
 
-// give the indicatrix its shape
+        // give the indicatrix its shape. y and z are swapped in unity!
         _transform = this.GetComponent<Transform>();
-        transform.localScale = new Vector3(alpha, beta, gamma);
-
+        transform.localScale = new Vector3(RIs[0], RIs[2], RIs[1]);
     }
 
-    void 
+    // adapted from http://gyanendushekhar.com/2018/01/11/rotate-gameobject-using-mouse-drag-or-touch-unity-tutorial/
+    void OnMouseDrag()
+    {
+        float XaxisRotation = Input.GetAxis("Mouse X") * rotationSpeed;
+        float YaxisRotation = Input.GetAxis("Mouse Y") * rotationSpeed;
+        _transform.Rotate(Vector3.down * XaxisRotation, Space.World);
+        _transform.Rotate(Vector3.right * YaxisRotation, Space.World);
+    }
 
     void Start()
     {
---
+++ Assets/Scripts/RenderLineToMe.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RenderLineToMe : MonoBehaviour
+{
+    public Vector3 origin = Vector3.zero;
+    private LineRenderer _lineRenderer;
+
+    private void OnEnable()
+    {
+        _lineRenderer = this.GetComponent<LineRenderer>();
+        _lineRenderer.SetPosition(0, origin);
+        _lineRenderer.SetPosition(1, this.transform.position);
+
+    }
+}
