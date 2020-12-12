756f52bf1c996552bf340f3e6a51a215b3e54646
DOD14 <38224694+DOD14@users.noreply.github.com>
2020-12-12 20:10:29 +0200

    created indicatrix object and set scale based on RIs

--- Assets/Scripts/Indicatrix.cs
+++ Assets/Scripts/Indicatrix.cs
@@ -4,13 +4,43 @@ using UnityEngine;
 
 public class Indicatrix : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public float alpha = 1;
+    public float beta = 2;
+    public float gamma = 3;
+
+    private Transform _transform;
+
+    private void OnEnable()
+    {
+
+// enforce alpha <= beta <= gamma
+        if (alpha > beta)
+        {
+            float temp = alpha;
+            alpha = beta;
+            beta = temp;
+        }
+
+        if(gamma < beta)
+        { float temp = beta;
+            beta = gamma;
+            gamma = temp;
+        }
+
+// give the indicatrix its shape
+        _transform = this.GetComponent<Transform>();
+        transform.localScale = new Vector3(alpha, beta, gamma);
+
+    }
+
+    void 
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
---
+++ Assets/Scripts/RI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RI : MonoBehaviour
+{
+    public Vector3 _direction = new Vector3(1, 0, 0);
+    private LineRenderer _lineRenderer;
+    private float _RI;
+   
+
+    private void OnEnable()
+    {
+        _lineRenderer = this.GetComponent<LineRenderer>();
+    }
+
+    private void SetLineRendererPoints()
+    {
+        Vector3 arrowHead = _direction * _RI;
+        _lineRenderer.SetPosition(1, arrowHead);
+    }
+
+    public void SetRI(float RI)
+    {
+        _RI = RI;
+    }
+}
